cfb50f0a8aab4e2a8b47449cfa345366fcd85e32
sathwik123677 <sathwikp06@gmail.com>
2026-06-08 22:08:22 +0530

    front-end code except login page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,6 +6,11 @@ import CarsDetails from "./pages/CarsDetails";
 import Cars from "./pages/Cars";
 import MyBookings from "./pages/MyBookings";
 import Footer from "./components/Footer"
+import Layout from "./pages/Owner/Layout";
+import AddCar from "./pages/Owner/AddCar";
+import Dashboard from "./pages/Owner/Dashboard";
+import ManageCars from "./pages/Owner/ManageCars";
+import ManageBookings from "./pages/Owner/ManageBookings";
 const App = () => {
    const [showLogin, setShowLogin] = useState(false);
    const location = useLocation();
@@ -13,12 +18,21 @@ const App = () => {
   return (
     <>
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/car-details/:id" element={<CarsDetails />} />
-      <Route path="/cars" element={<Cars />} />
-      <Route path="/my-bookings" element={<MyBookings />} />
-      </Routes>
+  
+     <Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/car-details/:id" element={<CarsDetails />} />
+  <Route path="/cars" element={<Cars />} />
+  <Route path="/my-bookings" element={<MyBookings />} />
+
+  <Route path="/owner" element={<Layout />}>
+    <Route index element={<Dashboard />} />
+    <Route path="add-car" element={<AddCar />} />
+    <Route path="manage-cars" element={<ManageCars />} />
+    <Route path="manage-bookings" element={<ManageBookings />} />
+  </Route>
+</Routes>
+      
      {!isOwnerPath && <Footer />}
     </>
   );
