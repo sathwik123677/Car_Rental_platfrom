08b42199b9a57a6fb3d6edfeb8b9f0a9a21eb8a6
sathwik123677 <sathwikp06@gmail.com>
2026-06-12 22:34:54 +0530

    remaining changes

--- Client/src/pages/owner/Layout.jsx
+++ Client/src/pages/owner/Layout.jsx
@@ -10,7 +10,7 @@ const Layout = () => {
     if(!isOwner){
       navigate('/');
     }
-  },[isOwner])
+  },[isOwner, navigate])
   return (
     <div className='flex flex-col'>
       <NavbarOwner />
